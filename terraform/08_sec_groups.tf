2a1fb05551ce771b499f781327c529c7b9f969fe
Pondpdpr <pBoonnud@gmail.com>
2024-03-19 01:36:21 +0700

    Revert "test"
    This reverts commit 52d167730156b381817696af7d44f17aa83f6c69.

--- terraform/08_sec_groups.tf
+++ terraform/08_sec_groups.tf
@@ -1,26 +1,26 @@
-resource "aws_security_group" "default" {
-    name        = "cc-midterm-default-sg"
-    description = "Allow all traffic within the VPC"
-    vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "default" {
+#     name        = "cc-midterm-default-sg"
+#     description = "Allow all traffic within the VPC"
+#     vpc_id      = aws_vpc.main.id
     
-    ingress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     ingress {
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     egress {
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    tags = {
-        Name = "cc-midterm-default-sg"
-    }
-}
+#     tags = {
+#         Name = "cc-midterm-default-sg"
+#     }
+# }
 
 resource "aws_security_group" "wp_server" {
     name        = "cc-midterm-wordpress-sg"
@@ -76,28 +76,32 @@ resource "aws_security_group" "db_server" {
         from_port   = 3306
         to_port     = 3306
         protocol    = "tcp"
-        security_groups = [aws_security_group.wp_server.id]
+        # security_groups = [aws_security_group.wp_server.id]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        security_groups = [aws_security_group.wp_server.id]
+        # security_groups = [aws_security_group.wp_server.id]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port = -1
         to_port = -1
         protocol = "icmp"
-        security_groups = [aws_security_group.wp_server.id]
+        # security_groups = [aws_security_group.wp_server.id]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        security_groups = [aws_security_group.wp_server.id]
+        # security_groups = [aws_security_group.wp_server.id]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
--- terraform/12_ec2.tf
+++ terraform/12_ec2.tf
@@ -8,10 +8,10 @@ resource "aws_instance" "db" {
         network_interface_id = aws_network_interface.db_to_nat.id
     }
     
-    # network_interface {
-    #     device_index = 1
-    #     network_interface_id = aws_network_interface.db_from_wp.id
-    # }
+    network_interface {
+        device_index = 1
+        network_interface_id = aws_network_interface.db_from_wp.id
+    }
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -38,14 +38,14 @@ resource "aws_instance" "db" {
 
 }
 
-resource "aws_network_interface_attachment" "db_from_wp" {
-    device_index = 1
-    instance_id = aws_instance.db.id
-    network_interface_id = aws_network_interface.db_from_wp.id
-}
+# resource "aws_network_interface_attachment" "db_from_wp" {
+#     device_index = 1
+#     instance_id = aws_instance.db.id
+#     network_interface_id = aws_network_interface.db_from_wp.id
+# }
 
 resource "aws_instance" "wp_server" {
-    depends_on = [ aws_instance.db, aws_network_interface_attachment.db_from_wp, aws_iam_access_key.s3_user ]
+    # depends_on = [ aws_instance.db, aws_network_interface_attachment.db_from_wp, aws_iam_access_key.s3_user ]
 
     ami = var.ami
     instance_type = "t2.micro"
@@ -55,10 +55,10 @@ resource "aws_instance" "wp_server" {
         device_index = 0
         network_interface_id = aws_network_interface.wp_internet.id
     }
-    # network_interface {
-    #     device_index = 1
-    #     network_interface_id = aws_network_interface.wp_to_db.id
-    # }
+    network_interface {
+        device_index = 1
+        network_interface_id = aws_network_interface.wp_to_db.id
+    }
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -126,8 +126,8 @@ resource "aws_instance" "wp_server" {
     }
 }
 
-resource "aws_network_interface_attachment" "wp_to_db" {
-    device_index = 1
-    instance_id = aws_instance.wp_server.id
-    network_interface_id = aws_network_interface.wp_to_db.id
-}
+# resource "aws_network_interface_attachment" "wp_to_db" {
+#     device_index = 1
+#     instance_id = aws_instance.wp_server.id
+#     network_interface_id = aws_network_interface.wp_to_db.id
+# }
